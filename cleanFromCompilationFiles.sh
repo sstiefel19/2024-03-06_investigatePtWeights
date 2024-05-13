3fb21623d56e9bd85335025ce489fc4ec57dc04e
Stephan Stiefelmaier <stiefelmaier@physi.uni-heidelberg.de>
2024-05-13 23:47:41 +0200

    med3

--- cleanFromCompilationFiles.sh
+++ cleanFromCompilationFiles.sh
@@ -1,16 +1,3 @@
-utilsDir="/analysisSoftware/utils_sstiefel_2024"
-thisDir=`pwd`
-
-for dir in "$utilsDir $thisDir"; do
-    echo "current dir is $dir"
-    if [ -n "$dir" ]; then
-        trgDir="${dir}/dir_moved_compilation_files"
-        mkdir $trgDir
-        mv $dir/*/*/*.d */*/*.so */*/*.pcm */*/*ACLiC* ${trgDir}/.
-        mv $dir/*.d *.so *.pcm *ACLiC* ${trgDir}/.
-        echo "moved to ${trgDir}. ls:"
-        echo `ls ${trgDir}`
-    fi
-done
-
+rm */*/*.d */*/*.so */*/*.pcm */*/*ACLiC*
+rm  *.d *.so *.pcm *ACLiC*
 
